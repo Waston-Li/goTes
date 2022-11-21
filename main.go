908aae46175453dd3a6159a1b035871c3e087de1
wangqianli <wangqianli@kylinos.cn>
2022-11-21 15:02:56 +0800

    分支推送远端测试

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"goTes/pack"
-	"io"
 	"log"
-	"net"
 	"os"
 	"runtime"
 	"strings"
@@ -42,28 +40,9 @@ func main() {
 	//pack.PowByChannel(arr)
 	//routineL.TicktoBoom()
 	//routineL.CSTest()
-
+	//这是版本分支
 	//网络、模板、与网页应用
-	var (
-		host          = "www.apache.org"
-		port          = "80"
-		remote        = host + ":" + port
-		msg    string = "GET / \n"
-		data          = make([]uint8, 4096)
-		read          = true
-		count         = 0
-	)
-	// 创建一个 socket
-	con, err := net.Dial("tcp", remote)
-	// 发送我们的消息，一个 http GET 请求
-	io.WriteString(con, msg)
-	// 读取服务器的响应
-	for read {
-		count, err = con.Read(data)
-		read = (err == nil)
-		fmt.Printf(string(data[0:count]))
-	}
-	con.Close()
+
 }
 
 func Setpcmd(c *pack.CMD) {
